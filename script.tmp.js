5d25a2dd7562bfd16423a25e1e64b0403d97e127
625alex <625alex@gmail.com>
2015-01-04 23:15:55 -0700

    Update script.tmp.js

--- script.tmp.js
+++ script.tmp.js
@@ -212,7 +212,7 @@ function updateTile(data) {
 		if (data.type == "music") {
 			if (data.trackDescription != tile.attr("data-track-description") || data.mute != tile.attr("data-mute")) spinner(tile);
 			tile.attr("data-track-description", data.trackDescription);
-			if (tile.attr("mute" != data.mute)) {tile.toggleClass("muted");}
+			if (tile.attr("mute") != data.mute) {tile.toggleClass("muted");}
 			tile.attr("mute", data.mute);
 			tile.find(".title .track").html(tile.attr("data-track-description"));
 		}
