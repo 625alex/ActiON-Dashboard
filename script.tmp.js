d766b28fa45acb4d9c10cd8cbb8541726c6b7b36
625alex <625alex@gmail.com>
2015-02-04 23:31:34 -0700

    undo

--- script.tmp.js
+++ script.tmp.js
@@ -21,7 +21,7 @@ $(function() {
 	$(".dashboard").click(function(e) {
 		animateClick($(this));
 		e.stopImmediatePropagation();
-		doURLSwap($(this).find("a").attr("href"));
+		window.location = $(this).find("a").attr("href");
 	});
 	
 	$(".switch, .light, .lock, .momentary, .holiday, .camera").click(function() {
@@ -294,12 +294,6 @@ function doRefresh() {
 	});
 }
 
-function doURLSwap(url) {
-	doPoll(function func(){
-		$(".refresh .icon").addClass("fa-spin");
-		window.location = url;
-	});
-}
 refresh(60 * 60); // hard refresh every hour
 
 function getUrlParameter(sParam)
